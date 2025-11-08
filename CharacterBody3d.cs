3d004b8ad5b4286774dac9842a822ef40ee79622
Solomon Walker <solomonmwalker@gmail.com>
2025-11-07 21:17:32 -0500

    first real commit

---
+++ CharacterBody3d.cs
@@ -0,0 +1,56 @@
+using Godot;
+using System;
+
+public partial class CharacterBody3d : CharacterBody3D
+{
+    public Camera3D camera;
+    public float cameraSensitivity = 0.01f;
+    public float speed = 10;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        camera = GetNode<Camera3D>("Camera3D");
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+        var movementInput = Vector2.Zero;
+        if (Input.IsKeyPressed(Key.W)) //forward
+        {
+            movementInput += Vector2.Up;
+        }
+        if (Input.IsKeyPressed(Key.S)) //backward
+        {
+            movementInput += Vector2.Down;
+        }
+        if (Input.IsKeyPressed(Key.D)) //right
+        {
+            movementInput += Vector2.Right;
+        }
+        if (Input.IsKeyPressed(Key.A)) //left
+        {
+            movementInput += Vector2.Left;
+        }
+        
+        //add in jump stuff https://git.colormatic.org/ColormaticStudios/quality-godot-first-person/src/branch/main/addons/fpc/character.gd
+
+        var directionV2 = movementInput.Rotated(-camera.Rotation.Y);
+        var directionV3 = new Vector3(directionV2.X, 0, directionV2.Y);
+        Velocity = directionV3 * speed;
+        MoveAndSlide();
+    }
+
+    public override void _UnhandledInput(InputEvent @event)
+    {
+        base._UnhandledInput(@event);
+        if (@event is InputEventMouseMotion mouseMotionEvent)
+        {
+            var lookDir = mouseMotionEvent.Relative;
+            var rotationY = camera.Rotation.Y - lookDir.X * cameraSensitivity;
+            var rotationX = Math.Clamp(camera.Rotation.X - lookDir.Y * cameraSensitivity, Mathf.DegToRad(-90), Mathf.DegToRad(90));
+            camera.SetRotation(new Vector3(rotationX, rotationY, 0));
+        }
+    }
+}
