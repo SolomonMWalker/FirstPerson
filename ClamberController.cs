66a2fe695e369a50a48bece1c56246da763bbc48
SolomonMWalker <solomonmwalker@gmail.com>
2025-11-25 20:18:55 -0500

    widened clamber controller raycasts

--- ClamberController.cs
+++ ClamberController.cs
@@ -12,11 +12,10 @@ public partial class ClamberController : Node3D
     [Export] public float raycastLength = 0.25f;
     [Export] public float clamberMargin = 0.26f;
     [Export] public float maxAngleInDeg = 10f;
-    [Export] public float waitPerCallInSec = 0.15f;
+    [Export] public float waitPerCallInSec = 0.25f;
 
     private double _timeSinceLastClamberCall = 0;
-    private RayCast3D _topRaycast;
-    private List<RayCast3D> _raycasts = [];
+    private List<List<RayCast3D>> _raycasts = [];
     private Node3D _raycastsParent;
     private Vector2 _topLeftCorner;
 
@@ -26,10 +25,15 @@ public partial class ClamberController : Node3D
         _raycastsParent = GetNode<Node3D>("Raycasts");
         foreach (var child in _raycastsParent.GetChildren())
         {
-            _raycasts.Add((RayCast3D)child);
+            List<RayCast3D> rcList = [];
+            foreach (var rc in child.GetChildren())
+            {
+                rcList.Add((RayCast3D)rc);
+            }
+
+            _raycasts.Add(rcList);
         }
 
-        _topRaycast = _raycasts.OrderByDescending(rc => rc.GlobalPosition.Y).First();
         GD.Print($"Built raycasts with {_raycasts.Count} rays");
     }
 
@@ -39,31 +43,11 @@ public partial class ClamberController : Node3D
         _timeSinceLastClamberCall += delta;
         _timeSinceLastClamberCall = Mathf.Clamp(_timeSinceLastClamberCall, 0, waitPerCallInSec);
     }
-
-
-    private void BuildRaycasts()
-    {
-        for (int i = 0; i < numRaycastsWide; i++)
-        {
-            for (int j = numRaycastsHigh; j > 0; j--)
-            {
-                var ray = new RayCast3D();
-                ray.Enabled = true;
-                ray.Position = new Vector3(
-                    _topLeftCorner.X + (i / (float)numRaycastsWide) * width,
-                    _topLeftCorner.Y - (i / (float)numRaycastsHigh) * height,
-                    0f);
-                ray.TargetPosition = ray.Position + new Vector3(0, 0, -raycastLength);
-                _raycasts.Add(ray);
-                _raycastsParent.AddChild(ray);
-            }
-        }
-    }
-
-    public List<(Vector2 localSlice, Vector3 globalEndpoint, bool collided)> GetRaycastEndPoints()
+    
+    public List<(Vector2 localSlice, Vector3 globalEndpoint, bool collided)> GetRaycastEndPoints(List<RayCast3D> raycasts)
     {
         List<(Vector2, Vector3, bool)> collisions = [];
-        foreach (var rc in _raycasts)
+        foreach (var rc in raycasts)
         {
             //switch this shit to 2d with global z and y
             if (rc.IsColliding())
@@ -88,12 +72,9 @@ public partial class ClamberController : Node3D
         return collisions;
     }
     
-
-    public (bool success, RaycastCollisionResult result) AttemptClamber()
+    private (bool success, RaycastCollisionResult result) AttemptClamberCheckRow(List<RayCast3D> raycasts)
     {
-        if (_timeSinceLastClamberCall < waitPerCallInSec) return (false, null);
-        _timeSinceLastClamberCall = 0;
-        var rawCollisions = GetRaycastEndPoints();
+        var rawCollisions = GetRaycastEndPoints(raycasts);
         if (rawCollisions.All(c => !c.collided)) return (false, null);
 
         //If top raycast is colliding, we can't clamber
@@ -117,6 +98,18 @@ public partial class ClamberController : Node3D
         //took out extra "check for angle" code for now
     }
 
+    public (bool success, RaycastCollisionResult result) AttemptClamber()
+    {
+        if (_timeSinceLastClamberCall < waitPerCallInSec) return (false, null);
+        _timeSinceLastClamberCall = 0;
+        foreach (var rcList in _raycasts)
+        {
+            var clamberAttemptRow = AttemptClamberCheckRow(rcList);
+            if (clamberAttemptRow.success) return clamberAttemptRow;
+        }
+
+        return (false, null);
+    }
 }
 
 public class RaycastCollisionResult
