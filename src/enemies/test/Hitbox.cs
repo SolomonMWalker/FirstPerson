b5bbd3b40b3195ed851563586d55e1e0cfcaf2a2
Solomon Walker <solomonmwalker@gmail.com>
2026-02-24 21:59:38 -0500

    lots of moving around

---
+++ src/enemies/test/Hitbox.cs
@@ -0,0 +1,48 @@
+using Godot;
+using System;
+using FirstPerson.scenes.enemies.test;
+
+[GlobalClass]
+public partial class Hitbox : Area3D
+{
+    [Export] public bool IsDebug { get; private set; }
+    
+    [ExportCategory("References")]
+    [Export] public HealthComponent HealthComponent { get; private set; }
+    [Export] public MeshInstance3D DebugMesh { get; private set; }
+    [Export] public Node3D Parent { get; private set; }
+
+    private Timer _debugMeshTimer;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        if (IsDebug)
+        {
+            _debugMeshTimer = new Timer();
+            _debugMeshTimer.SetWaitTime(1);
+            _debugMeshTimer.Autostart = false;
+            _debugMeshTimer.Timeout += () =>
+            {
+                DebugMesh.SetVisible(false);
+            };
+            AddChild(_debugMeshTimer);
+        }
+    }
+
+    public void Hit(HitInformation hitInformation)
+    {
+        if (hitInformation.healthDamage.HasValue)
+        {
+            HealthComponent?.DepleteHealth(hitInformation.healthDamage.Value);
+            if(IsDebug) DebugHit();
+        }
+    }
+
+    public void DebugHit()
+    {
+        _debugMeshTimer.Stop();
+        DebugMesh.SetVisible(true);
+        _debugMeshTimer.Start();
+    }
+}
