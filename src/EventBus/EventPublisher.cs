eb80bb9d09d072166841eecf5c9e632db4ba1dd3
Solomon Walker <solomonmwalker@gmail.com>
2026-02-27 15:14:10 -0500

    more removals

--- src/EventBus/EventPublisher.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FirstPerson.EventBus;
-
-public class EventPublisher
-{
-    public bool TryPublishEvent(string eventName, object parameters) =>
-        EventBus.TryPublishEvent(eventName, parameters);
-
-    public bool TryCreateEvent(string eventName) => EventBus.TryCreateEvent(eventName);
-}
